7cf1043823e4431fc29455378cff82a594b759ad
Adhitya Vadivel <adhitya.vadivel@gmail.com>
2015-10-17 11:49:17 -0400

    Initial Commit

---
+++ BuffBro/SecondViewController.swift
@@ -0,0 +1,25 @@
+//
+//  SecondViewController.swift
+//  BuffBro
+//
+//  Created by Apple on 10/17/15.
+//  Copyright © 2015 Adhitya Vadivel. All rights reserved.
+//
+
+import UIKit
+
+class SecondViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
